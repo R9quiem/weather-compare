7ded3f55a9f2412bd4e5dec2fdf19642e4d75f56
Efim (laptop) <efimburachevsky@gmail.com>
2026-06-18 16:52:00 +0300

    feat(frontend): display all cities from db on weather page

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,14 +1,10 @@
 const API_URL = "http://127.0.0.1:8000";
 
-async function getCities() {
-    const response = await fetch(
-        `${API_URL}/cities`
-    );
+export async function getCities() {
+    const response = await fetch(`${API_URL}/cities`);
 
     if (!response.ok) {
-        throw new Error(
-            `Ошибка запроса: ${response.status}`
-        );
+        throw new Error(`Ошибка запроса: ${response.status}`);
     }
 
     const data = await response.json();
--- frontend/src/pages/CityWeatherPage.jsx
+++ frontend/src/pages/CityWeatherPage.jsx
@@ -1,14 +1,14 @@
 import TemperatureChart from "../components/charts/TemperatureChart";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import { getCities } from "../api.jsx";
 
 function CityWeatherPage() {
-
     const [cities, setCities] = useState([]);
     const [dailyWeather, setDailyWeather] = useState([])
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
-  useEffect(() => {
+    useEffect(() => {
         async function loadCities() {
             try {
                 setIsLoading(true);
@@ -25,14 +25,26 @@ function CityWeatherPage() {
         }
 
         loadCities();
-
     }, []);
 
-  return (
-    <div>
-      <h1>City weather</h1>
-      <TemperatureChart />
-    </div>
-  );
+    return (
+        <div>
+            <h1>City weather</h1>
+            {isLoading && <p>Загрузка городов...</p>}
+
+            {error && <p>Ошибка: {error}</p>}
+
+            {!isLoading && !error && (
+                <ul>
+                    {cities.map((city) => (
+                        <li key={city.id}>
+                            {city.name}, {city.country_code}
+                        </li>
+                    ))}
+                </ul>
+            )}
+            <TemperatureChart />
+        </div>
+    );
 }
-export default CityWeatherPage
+export default CityWeatherPage;
