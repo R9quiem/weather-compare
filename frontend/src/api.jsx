449f9dc259a347c7da951e3f7cc6e34176d58df8
efim <efimburachevsky@gmail.com>
2026-06-18 14:11:16 +0300

    feat(frontend): fetch cities

---
+++ frontend/src/api.jsx
@@ -0,0 +1,17 @@
+const API_URL = "http://127.0.0.1:8000";
+
+async function getCities() {
+    const response = await fetch(
+        `${API_URL}/cities`
+    );
+
+    if (!response.ok) {
+        throw new Error(
+            `Ошибка запроса: ${response.status}`
+        );
+    }
+
+    const data = await response.json();
+
+    return data;
+}
--- frontend/src/pages/CityWeatherPage.jsx
+++ frontend/src/pages/CityWeatherPage.jsx
@@ -1,10 +1,37 @@
 import TemperatureChart from "../components/charts/TemperatureChart";
+import {useEffect, useState} from "react";
 
 function CityWeatherPage() {
+
+    const [cities, setCities] = useState([]);
+    const [dailyWeather, setDailyWeather] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+  useEffect(() => {
+        async function loadCities() {
+            try {
+                setIsLoading(true);
+                setError(null);
+
+                const loadedCities = await getCities();
+
+                setCities(loadedCities);
+            } catch (requestError) {
+                setError(requestError.message);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        loadCities();
+
+    }, []);
+
   return (
     <div>
       <h1>City weather</h1>
-      <TemperatureChart data={chartData} />
+      <TemperatureChart />
     </div>
   );
 }
